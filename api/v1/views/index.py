4962e3b425445e7e04d8f2e0fb8e3e0d7a9602d0
Mikaelia <mikaelagurney@gmail.com>
2018-08-21 17:45:19 +0000

    Add object count view function to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,29 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route("/status")
 def status():
-    """ The "status" route.
-        Returns "status": "OK" json
+    """
+    The "status" route.
+    Returns "status": "OK" json
     """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def obj_count():
+    """
+    Returns the count of each object
+    """
+    obj_dict = {} #
+    obj_names = {"Amenity": "amenites",
+         "City": "cities",
+         "Place": "places",
+         "Review": "reviews",
+         "State": "states",
+         "User": "users" }
+
+    for k, v in obj_names.items():
+        obj_dict[v] = storage.count(k)
+    return jsonify(obj_dict)
