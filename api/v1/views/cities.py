fc122648dc9cc377011383dc605cb214270fa1bc
Mikaelia <mikaelagurney@gmail.com>
2018-08-22 18:27:17 +0000

    Refactor all city functions. All currently run without server errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,79 +1,83 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from models import storage, City
-from flask import abort, request
+from flask import abort, request, jsonify
 
-@app_views.route('/api/v1/states/<state_id>/cities')
+@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['GET'])
 def city_by_state_id(state_id):
     """
     Retrieves list of all City objects of a State
     """
-    city_list = []
-    cities = storage.all(City)
-    city_list = [to_dict(city) for city in cities if city['state_id'] == state_id]
+    cities = storage.all("City").values()
+    city_list = [city.to_dict() for city in cities if city.state_id == state_id]
     if len(city_list) == 0:
             abort(404)
-    return city_list
-@app_views.route('/api/v1/cities/<city_id>')
+    return jsonify(city_list)
+
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['GET'])
 def city_by_id(city_id):
     """
     Retrieves city by city id. If city_id not linked to City,
     raise 404.
     """
-    my_city = None
-    cities = storage.all(City)
-    city = next(filter(lambda x: x["id"] == city_id, cities), None)
-    return to_dict(city) if item else abort(404)
+    cities = storage.all("City").values()
+    city = next(filter(lambda x: x.id == city_id, cities), None)
+    return jsonify(city.to_dict()) if city else abort(404), 201
 
-@app_views.route('/api/v1/cities/<city_id>')
-def delete_city():
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['DELETE'])
+def delete_city(city_id):
     """
     Deletes city by id. If city_id not linked to City, raise 404
     Returns empty dict with status 200
     """
-    cities = storage.all(City)
-    city = next(filter(lambda x: x["id"] == city_id, cities), None)
+    cities = storage.all("City").values()
+    city = next(filter(lambda x: x.id == city_id, cities), None)
     if city:
         storage.delete(city)
-        return {}, 200
+        return jsonify({}), 200
     else:
         abort(404)
 
-@app_views.route('/api/v1/states/<state_id>/cities')
-def create_city():
+@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['POST'])
+def create_city(state_id):
     """
     Creates new city. If request body not valid JSON, raises 400
     If state_id not linked to State, raise 404
     If dict does not contain 'name' key, raise 400
     Returns city object with status 201
     """
-    if not request.json or not 'name' in request.json:
+    if not request.get_json() or not 'name' in request.get_json():
         abort(400)
-    city = {
-        'state_id': request.json['state_id'],
-        'name': request.json['name'],
-        'places': request.json['places']
-    }
-    storage.new(city)
-    storage.save(city)
-    return to_dict(city), 201
-#need to check if state id linked to state**
 
-@app_views.route('/api/v1/cities/<city_id>')
+    a = storage.get("State", state_id)
+    if a is None:
+        abort(404)
+
+    kwargs = request.get_json()
+    kwargs['state_id'] = state_id
+    my_city = City(**kwargs)
+    storage.new(my_city)
+    storage.save()
+    return jsonify(my_city.to_dict()), 201
+   
+
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['PUT'])
 def put(city_id):
     """
     Updates city. If request not valid JSON, raises 400.
     If city_id not linked to City object, raise 404
     Returns city object with status code 200
     """
-    cities = storage.all(City)
-    city = [city for city in cities if city['id'] == city_id]
-    if len(city) == 0:
+    cities = storage.all("City").values()
+    city = next(filter(lambda x: x.id == city_id, cities), None)
+    if not city:
             abort(404)
-    if not request.json:
+    if not request.get_json():
             abort(400)
-    city[0]['state_name'] = request.json.get('state_name', city[0]['state_name'])
-    city[0]['name'] = request.json.get('name', city[0]['name'])
-    city[0]['places'] = request.json.get('places', city[0]['places'])
+    args = request.get_json()
 
-    return to_dict(city[0]), 200
+    city.state_name = args.get('state_id', city.state_id)
+    city.name = args.get('name', city.name)
+    city.places = args.get('places', city.places)
+
+    return jsonify(city.to_dict()), 200
