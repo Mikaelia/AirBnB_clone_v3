a528961747056843e735de393a9f5f150f5c7784
Mikaelia <mikaelagurney@gmail.com>
2018-08-23 21:55:03 +0000

    Move location of blueprint file

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,9 +10,9 @@ from api.v1.views import app_views
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
