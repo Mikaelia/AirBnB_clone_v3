d7c46e7a7a9764d4b0929f442ee013121a295933
Cu7ious <328@holbertonschool.com>
2018-08-23 20:50:50 +0000

    Added threaded=True

--- api/v1/app.py
+++ api/v1/app.py
@@ -33,4 +33,4 @@ def not_found_error(error):
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
