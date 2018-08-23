766aeaae2393332340d3e33e8d47dd855dbd5fc3
Cu7ious <328@holbertonschool.com>
2018-08-23 20:07:20 +0000

    Made blueprint more explicit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,7 @@ This module initilizes the "app_views" blueprint
 """
 from flask import Blueprint
 
-app_views = Blueprint("/api/v1", __name__)
+app_views = Blueprint("app_views", __name__,  url_prefix="/api/v1")
 if app_views is not None:
     from api.v1.views.index import *
     from api.v1.views.states import *
