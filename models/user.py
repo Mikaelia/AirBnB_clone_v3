d3b01c75959d81497292ab7c79b5f9bff308f880
Cu7ious <328@holbertonschool.com>
2018-08-24 04:45:24 +0000

    Added password hashing to the User model

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
     Implementation of the User class which inherits from BaseModel
 '''
 from os import getenv
+from hashlib import md5
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
@@ -13,9 +14,20 @@ class User(BaseModel, Base):
         Definition of the User class
     '''
     __tablename__ = "users"
+
+    def __init__(self, **kwargs):
+        """ L"""
+        super().__init__(**kwargs)
+        self.password = self.set_password(self.password)
+        # print("\n", "super.__dict__", super().__dict__, "\n")
+
+    def set_password(self, plaintext):
+        """ U"""
+        return md5(plaintext.encode()).hexdigest().lower()
+
     if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
-        email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
+        email = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user",
